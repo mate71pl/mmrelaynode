a053ba05275b56d5d1cf88c33c617b9c746a3427
mate-dev <mmaati@interia.pl>
2023-08-20 20:55:41 +0000

    Add arm builder

---
+++ build-native.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+
+set -e
+
+VERSION=`bin/buildinfo.py long`
+SHORT_VERSION=`bin/buildinfo.py short`
+
+OUTDIR=release/
+
+rm -f $OUTDIR/firmware*
+
+mkdir -p $OUTDIR/
+rm -r $OUTDIR/* || true
+
+# Important to pull latest version of libs into all device flavors, otherwise some devices might be stale
+platformio pkg update 
+
+pio run --environment linux-arm
+cp .pio/build/native/program $OUTDIR/meshtasticd_linux_arm
+
+cp bin/device-install.* $OUTDIR
+cp bin/device-update.* $OUTDIR
