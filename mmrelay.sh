bf204d2d9f9eac661bb19943ddc11aa186a90ab0
mate-dev <67105053+mate-dev@users.noreply.github.com>
2023-09-17 08:36:58 +0000

    add temp file

--- mmrelay.sh
+++ mmrelay.sh
@@ -28,15 +28,100 @@ if ! docker &> /dev/null; then
     rm get-docker.sh
 fi
 
-# Sprawdzamy, czy docker działa poprzez listowanie kontenerów
-if ! docker ps | grep -q "CONTAINER"; then
-    echo "================================================================================"
-    echo "If you see this error, type 'newgrp docker' and check if 'docker ps' returns an error."
-    echo "Or log out and log in for the changes to take effect."
-    echo "After this re-enable the script."
-    exit 1
+# Function to display help message
+display_help() {
+    echo "Usage: $0 [command] (optional container_name)"
+    echo "Commands:"
+    echo "  build - Build containers (optional: specify container_name)"
+    echo "  up - Start containers (optional: specify container_name)"
+    echo "  start - Start containers (optional: specify container_name)"
+    echo "  down - Stop and remove containers (optional: specify container_name)"
+    echo "  destroy - Stop, remove containers, and remove volumes (optional: specify container_name)"
+    echo "  stop - Stop containers (optional: specify container_name)"
+    echo "  restart - Restart containers (optional: specify container_name)"
+    echo "  logs - Show container logs (optional: specify container_name)"
+    echo "  ps - List containers"
+    echo "  login-timescale - Log in to the TimescaleDB container"
+    echo "  login-api - Log in to the API container"
+    echo "  db-shell - Open a database shell in the TimescaleDB container"
+    echo "  menu - Enter to interactive menu"
+    echo "  --help, -h - Display this help message"
+}
+
+# ... (reszta funkcji pozostaje taka sama)
+
+# Check for the help flag
+if [[ "$1" == "--help" || "$1" == "-h" ]]; then
+    display_help
+    exit 0
 fi
 
-# Pozostała część skryptu
-echo "jedziemy"
-#docker compose -f "docker-compose.yaml" up -d --build
+# Check if "menu" is in the script name, and if so, display the menu
+if [[ $0 == *"menu"* ]]; then
+    while true; do
+        clear
+        echo "Bash Menu:"
+        echo "1. Build [container_name]"
+        echo "2. Up [container_name]"
+        echo "3. Start [container_name]"
+        echo "4. Down [container_name]"
+        echo "5. Destroy [container_name]"
+        echo "6. Stop [container_name]"
+        echo "7. Restart [container_name]"
+        echo "8. Logs [container_name]"
+        echo "9. PS"
+        echo "10. Login to TimescaleDB Container"
+        echo "11. Login to API Container"
+        echo "12. Database Shell (TimescaleDB)"
+        echo "0. Exit"
+        echo "--help, -h - Display this help message"
+
+        read -p "Enter your choice: " choice
+
+        case "$choice" in
+            1) read -p "Enter container name (or press Enter for default): " container_name
+               build "$container_name" ;;
+            2) read -p "Enter container name (or press Enter for default): " container_name
+               up "$container_name" ;;
+            3) read -p "Enter container name (or press Enter for default): " container_name
+               start "$container_name" ;;
+            4) read -p "Enter container name (or press Enter for default): " container_name
+               down "$container_name" ;;
+            5) read -p "Enter container name (or press Enter for default): " container_name
+               destroy "$container_name" ;;
+            6) read -p "Enter container name (or press Enter for default): " container_name
+               stop "$container_name" ;;
+            7) read -p "Enter container name (or press Enter for default): " container_name
+               restart "$container_name" ;;
+            8) read -p "Enter container name (or press Enter for default): " container_name
+               logs "$container_name" ;;
+            9) ps ;;
+            10) login-timescale ;;
+            11) login-api ;;
+            12) db-shell ;;
+            0) exit ;;
+            "--help" | "-h") display_help ;;
+            *) echo "Invalid choice. Please try again." ;;
+        esac
+
+        read -p "Press Enter to continue..."
+    done
+else
+    # If "menu" is not in the script name, execute the specified command
+    case "$1" in
+        "build") build "$2" ;;
+        "up") up "$2" ;;
+        "start") start "$2" ;;
+        "down") down "$2" ;;
+        "destroy") destroy "$2" ;;
+        "stop") stop "$2" ;;
+        "restart") restart "$2" ;;
+        "logs") logs "$2" ;;
+        "ps") ps ;;
+        "login-timescale") login-timescale ;;
+        "login-api") login-api ;;
+        "db-shell") db-shell ;;
+        "--help" | "-h") display_help ;;
+        *) echo "Invalid command. Use --help or -h for available commands." ;;
+    esac
+fi
