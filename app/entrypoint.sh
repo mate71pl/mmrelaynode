45ca0e2c43ced594f2517a2869229ff228312f08
Jeremiah K <17190268+jeremiah-k@users.noreply.github.com>
2023-11-26 15:51:34 -0600

    Big progress, can connect

--- app/entrypoint.sh
+++ app/entrypoint.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-/home/mesh/.local/bin/wait-for-it.sh mmrelaynode:4403 -t 60
+/home/mesh/.local/bin/wait-for-it.sh mmrelaydevice:4403 -t 60
 python3 command_wrapper.py
 python3 conf_wrapper.py
 python3 main.py
